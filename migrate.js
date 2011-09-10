55bed8a877a4a87e2a7c6aabac18fae2702af04b
Szymon Przybylski <szymon.przybylski@gmail.com>
2011-09-10 16:04:29 +0200

    pushing users and statuses to couchdb

--- migrate.js
+++ migrate.js
@@ -1,18 +1,35 @@
 var database_module = require('./database-mysql.js');
 var database = new database_module.Database();
 
+var cradle = require('cradle');
+var db = new(cradle.Connection)().database('tweet-couchdb');
+
+db.exists(function (err, exists) {
+	if (err) {
+	  console.log('error', err);
+	} else if (exists) {
+	  console.log('the force is with you.');
+	} else {
+	  console.log('database does not exists.');
+	}
+});
+
 console.log('Start migrate users');
 
 var users = [];
 var limit = 0;
 
 database.findUsers( function(users) {
-
+  var data = users;
   console.log('Users migrated. users.length:' + users.length);
   console.log('Start migrate statuses');
 
   for (var i=0; i < users.length; i++) {
     database.selectTweets(users[i].screen_name, function(x,tweets){
+      
+      console.log(tweets);
+      data[x].statuses = tweets;
+      db.save(data[x]);
       console.log(x);
       // insert user into couchdb
       // TODO insert users[x] + tweets
@@ -20,5 +37,7 @@ database.findUsers( function(users) {
       // tweets - tablica twittow
     }.bind(null,i));
   }
+  
+ 
 });
 
